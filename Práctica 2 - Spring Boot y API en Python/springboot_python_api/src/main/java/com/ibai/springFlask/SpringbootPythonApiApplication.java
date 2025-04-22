81ddf5e891a380e1706c2682883bdee26c034a64
Ibai <ibaimoya03@gmail.com>
2025-04-22 23:22:02 +0200

    Añadida la estructura de la PrÃáctica 2.

---
+++ Práctica 2 - Spring Boot y API en Python/springboot_python_api/src/main/java/com/ibai/springFlask/SpringbootPythonApiApplication.java
@@ -0,0 +1,13 @@
+package com.ibai.springFlask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPythonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPythonApiApplication.class, args);
+	}
+
+}
