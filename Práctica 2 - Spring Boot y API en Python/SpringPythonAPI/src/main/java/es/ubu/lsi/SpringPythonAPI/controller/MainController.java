23fb53b99824553aa11c7a66e2d7a7a34698ddfc
Ibai <ibaimoya03@gmail.com>
2025-04-22 23:47:49 +0200

    Añadidao controlador principal.

---
+++ Práctica 2 - Spring Boot y API en Python/SpringPythonAPI/src/main/java/es/ubu/lsi/SpringPythonAPI/controller/MainController.java
@@ -0,0 +1,17 @@
+package es.ubu.lsi.SpringPythonAPI.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String paginaPrincipal() {
+        return "index";  // Thymeleaf: src/main/resources/templates/index.html
+    }
+    @GetMapping("/login")
+    public String pantallaLogin() {
+        return "login";
+    }
+}
