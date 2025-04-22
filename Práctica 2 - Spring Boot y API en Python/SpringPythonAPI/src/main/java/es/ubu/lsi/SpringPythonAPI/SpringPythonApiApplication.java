179fd823bf6723d99ab0006af8eda3bc79a1c845
Ibai <ibaimoya03@gmail.com>
2025-04-22 23:42:49 +0200

    Añadida la estructura de la Práctica 2.

---
+++ Práctica 2 - Spring Boot y API en Python/SpringPythonAPI/src/main/java/es/ubu/lsi/SpringPythonAPI/SpringPythonApiApplication.java
@@ -0,0 +1,13 @@
+package es.ubu.lsi.SpringPythonAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPythonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPythonApiApplication.class, args);
+	}
+
+}
