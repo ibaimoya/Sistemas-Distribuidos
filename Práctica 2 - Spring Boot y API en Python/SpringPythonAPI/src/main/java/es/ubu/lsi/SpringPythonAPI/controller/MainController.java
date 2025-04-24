c37d6959208a64dc4e17bf8bd824a9cd80843fda
Ibai <ibaimoya03@gmail.com>
2025-04-24 20:46:37 +0200

    Arreglado posible bucle de inicio de sesión.

--- Práctica 2 - Spring Boot y API en Python/SpringPythonAPI/src/main/java/es/ubu/lsi/SpringPythonAPI/controller/MainController.java
+++ Práctica 2 - Spring Boot y API en Python/SpringPythonAPI/src/main/java/es/ubu/lsi/SpringPythonAPI/controller/MainController.java
@@ -12,13 +12,26 @@ import es.ubu.lsi.SpringPythonAPI.form.RegistroForm;
 @Controller
 public class MainController {
 
+    Principal principal = null;
+
     @GetMapping("/")
     public String paginaPrincipal() {
+
+        /* Impide volver a zonas para logearse / registrarse. */
+        if (this.principal != null) {
+            return "redirect:/menu";
+        }
+
         return "index";
     }
 
     @GetMapping("/register")
     public String pantallaRegistro(Model model) {
+
+        /* Impide volver a la zona para registrarse. */
+        if (this.principal != null) {
+            return "redirect:/menu";
+        }        
 
         if (!model.containsAttribute("registroForm")) {
             model.addAttribute("registroForm", new RegistroForm());
@@ -28,12 +41,21 @@ public class MainController {
 
     @GetMapping("/login")
     public String pantallaLogin() {
+
+        /* Impide volver a la zona para logearse. */
+        if (this.principal != null) {
+            return "redirect:/menu";
+        }
+
         return "login";
     }
 
     @GetMapping("/menu")
     public String pantallaMenu(Model model, Principal principal) {
         if (principal != null) {
+
+            this.principal = principal;
+
             model.addAttribute("usuarioActual", principal.getName());
         }
         return "menu";
