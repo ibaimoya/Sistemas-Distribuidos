a90f64ba2d4b1cc1ff29e8e06aeadf55bf16af05
Ibai <ibaimoya03@gmail.com>
2025-04-25 12:19:45 +0200

    Creada lista de autorrelleno.

--- Práctica 2 - Spring Boot y API en Python/SpringPythonAPI/flask-api/api.py
+++ Práctica 2 - Spring Boot y API en Python/SpringPythonAPI/flask-api/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 from flask_cors import CORS   # para permitir peticiones desde 8080
 import requests
 
@@ -6,6 +6,8 @@ app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})   # CORS abierto
 
 POKE_URL = "https://pokeapi.co/api/v2/pokemon/{}"
+POKE_LIST_URL = "https://pokeapi.co/api/v2/pokemon"
+
 
 @app.route("/api/pokemon/<name_or_id>")
 def get_pokemon(name_or_id: str):
@@ -15,6 +17,25 @@ def get_pokemon(name_or_id: str):
         return jsonify(r.json())
     except requests.exceptions.HTTPError:
         abort(404, "Pokémon no encontrado.")
+    except requests.exceptions.Timeout:
+        abort(504, "Tiempo de espera agotado.")
+    except Exception:
+        abort(500, "Error al contactar con PokeAPI.")
+
+@app.route("/api/pokemons")
+def get_pokemon_list():
+    # opcional: ?limit=100&offset=0
+    limit  = request.args.get("limit", 2000)
+    offset = request.args.get("offset", 0)
+    try:
+        r = requests.get(
+            f"{POKE_LIST_URL}?limit={limit}&offset={offset}",
+            timeout=4
+        )
+        r.raise_for_status()
+        return jsonify(r.json())
+    except requests.exceptions.HTTPError:
+        abort(502, "Error al obtener la lista de Pokémon.")
     except requests.exceptions.Timeout:
         abort(504, "Tiempo de espera agotado.")
     except Exception:
