13105e8bb68c7867680bae9cb5e55e59a7c61e04
Ibai <ibaimoya03@gmail.com>
2025-05-30 00:00:26 +0200

    Corregido error en UsuarioRepositoryTest.java.

--- Práctica 3 - Taller Final/web/src/test/java/es/ubu/lsi/web/repository/UsuarioRepositoryTest.java
+++ Práctica 3 - Taller Final/web/src/test/java/es/ubu/lsi/web/repository/UsuarioRepositoryTest.java
@@ -4,15 +4,22 @@ import es.ubu.lsi.web.entity.Usuario;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+import es.ubu.lsi.web.service.AuthService;
+import es.ubu.lsi.web.config.SecurityConfig;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@Transactional
+@DataJpaTest
+@ComponentScan(basePackageClasses = AuthService.class)
+@Import(SecurityConfig.class)
 class UsuarioRepositoryTest {
 
     @Autowired
