c4d5b594627817ecd1993aa70c78efef1f35fb51
ibai <142017599+ibaimoya@users.noreply.github.com>
2025-05-29 23:34:41 +0200

    Añadido nuevo test para la creación del primer usuario.

---
+++ Práctica 3 - Taller Final/web/src/test/java/es/ubu/lsi/web/WebApplicationTest.java
@@ -0,0 +1,37 @@
+package es.ubu.lsi.web;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
+
+import es.ubu.lsi.web.service.AuthService;
+
+class WebApplicationTest {
+
+    @Test
+    void testInitUserExists() throws Exception {
+        // Comprueba que no registra si el usuario ya existe.
+        AuthService auth = mock(AuthService.class);
+        when(auth.existeUsuario("admin", "admin@admin.com")).thenReturn(true);
+
+        CommandLineRunner runner = new WebApplication().init(auth);
+        runner.run();
+
+        verify(auth).existeUsuario("admin", "admin@admin.com");
+        verify(auth, never()).registrar(any(), any(), any());
+    }
+
+    @Test
+    void testInitRegisterUser() throws Exception {
+        // Comprueba que registra si el usuario no existe.
+        AuthService auth = mock(AuthService.class);
+        when(auth.existeUsuario("admin", "admin@admin.com")).thenReturn(false);
+
+        CommandLineRunner runner = new WebApplication().init(auth);
+        runner.run();
+
+        verify(auth).existeUsuario("admin", "admin@admin.com");
+        verify(auth).registrar("admin", "admin@admin.com", "admin");
+    }
+}
