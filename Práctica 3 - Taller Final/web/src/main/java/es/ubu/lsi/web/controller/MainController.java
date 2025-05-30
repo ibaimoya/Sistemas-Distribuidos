e39cf5c3745850a58e63ca05e5ccad751310d055
Ibai <ibaimoya03@gmail.com>
2025-05-30 12:27:42 +0200

    Corregida duplicación de código.

--- Práctica 3 - Taller Final/web/src/main/java/es/ubu/lsi/web/controller/MainController.java
+++ Práctica 3 - Taller Final/web/src/main/java/es/ubu/lsi/web/controller/MainController.java
@@ -10,13 +10,14 @@ import es.ubu.lsi.web.form.RegisterForm;
 
 @Controller
 public class MainController {
+    String REDIRECCION_HOME = "redirect:/home";
 
     @GetMapping("/")
     public String paginaPrincipal(Principal principal) {
 
         /* Impide volver a zonas para logearse / registrarse. */
         if (principal != null) {
-            return "redirect:/menu";
+            return REDIRECCION_HOME;
         }
 
         return "index";
@@ -27,7 +28,7 @@ public class MainController {
 
         /* Impide volver a la zona para registrarse. */
         if (principal != null) {
-            return "redirect:/menu";
+            return REDIRECCION_HOME;
         }
 
         if (!model.containsAttribute("registroForm")) {
@@ -41,17 +42,17 @@ public class MainController {
 
         /* Impide volver a la zona para logearse. */
         if (principal != null) {
-            return "redirect:/menu";
+            return REDIRECCION_HOME;
         }
 
         return "login";
     }
 
-    @GetMapping("/menu")
-    public String pantallaMenu(Model model, Principal principal) {
+    @GetMapping("/home")
+    public String pantallaHome(Model model, Principal principal) {
         if (principal != null) {
             model.addAttribute("usuarioActual", principal.getName());
         }
-        return "menu";
+        return "home";
     }
 }
